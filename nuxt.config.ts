33396bc1471ef1493de56a7c3cee6b67d0fb38f9
onmax <maximogarciamtnez@gmail.com>
2026-01-20 17:12:35 +0100

    add nuxthub with D1 for nuxt content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/content', '@nuxt/icon'],
+  modules: ['@nuxthub/core', '@nuxt/eslint', '@nuxt/ui', '@nuxt/content', '@nuxt/icon'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   routeRules: { '/': { prerender: true }, '/posts/**': { prerender: true } },
   compatibilityDate: '2025-01-15',
-  nitro: { preset: 'cloudflare', prerender: { crawlLinks: true } },
+  hub: { db: { dialect: 'sqlite', driver: 'd1', connection: { databaseId: '5a113631-37db-48a6-8520-e93358d2889d' } } },
+  nitro: { prerender: { crawlLinks: true } },
   eslint: {
     config: { stylistic: { commaDangle: 'never', braceStyle: '1tbs' } }
   },

304505a4c5130b82c4aac8283a08b232f250f135
onmax <maximogarciamtnez@gmail.com>
2026-01-27 13:19:23 +0100

    fix ci: ignore .wrangler, disable multi-word component names

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  ignores: ['.wrangler/**']
+}, {
+  rules: {
+    'vue/multi-word-component-names': 'off'
+  }
+})

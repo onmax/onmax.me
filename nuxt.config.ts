d7d5375159d18fc15e27b85850ca47633e927b17
onmax <maximogarciamtnez@gmail.com>
2026-02-16 20:12:21 +0100

    feat: configure nuxt/image optimization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ['@nuxthub/core', '@nuxt/eslint', '@nuxt/ui', '@nuxt/content', '@nuxt/icon'],
+  modules: ['@nuxthub/core', '@nuxt/eslint', '@nuxt/ui', '@nuxt/content', '@nuxt/icon', '@nuxt/image'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   // Source lives in /app (pages, components, app.vue, etc.)
@@ -15,5 +15,11 @@ export default defineNuxtConfig({
   eslint: {
     config: { stylistic: { commaDangle: 'never', braceStyle: '1tbs' } }
   },
-  icon: { serverBundle: 'remote' }
+  icon: { serverBundle: 'remote' },
+  image: {
+    provider: 'ipx',
+    quality: 80,
+    format: ['webp', 'avif'],
+    domains: ['onmax.me']
+  }
 })
